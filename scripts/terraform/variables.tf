28998119065a3c5dee19714ac7ebefd7bf2bf06f
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 13:17:28 +1100

    cred to terrafrom files

--- scripts/terraform/variables.tf
+++ scripts/terraform/variables.tf
@@ -9,3 +9,24 @@ variable location {
 variable kubernetes_version {  
   default = "1.30.2"  
 }
+
+# Define variables to store Azure credentials
+variable "client_id" {
+  type = string
+  description = "The Client ID (Application ID) for Azure authentication."
+}
+
+variable "client_secret" {
+  type = string
+  description = "The Client Secret for Azure authentication."
+}
+
+variable "subscription_id" {
+  type = string
+  description = "The Subscription ID for Azure authentication."
+}
+
+variable "tenant_id" {
+  type = string
+  description = "The Tenant ID for Azure authentication."
+}
