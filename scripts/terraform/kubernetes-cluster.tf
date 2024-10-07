1a19e679448d602af36ba632bdf588cad2aa25a3
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 17:11:09 +1100

    kube code

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -1,14 +1,15 @@
 # Check if the Kubernetes cluster exists
 data "azurerm_kubernetes_cluster" "existing_cluster" {
-  name = var.app_name
-  resource_group_name = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? azurerm_resource_group.flixtubeazurekeyvault[0].name : var.app_name
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name
+  depends_on          = [azurerm_resource_group.flixtubeazurekeyvault]  # Ensures resource group is created first
 }
 
 # Create the Kubernetes cluster only if it does not exist
 resource "azurerm_kubernetes_cluster" "cluster" {
   count               = length(data.azurerm_kubernetes_cluster.existing_cluster.id) == 0 ? 1 : 0
   name                = var.app_name
-  location            = var.location
+  location            = azurerm_resource_group.flixtubeazurekeyvault[0].location
   resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name
   dns_prefix          = var.app_name
   kubernetes_version  = var.kubernetes_version
@@ -22,23 +23,19 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   identity {
     type = "SystemAssigned"
   }
+
   depends_on = [azurerm_resource_group.flixtubeazurekeyvault]
 }
 
 # Assign Key Vault access to the AKS Managed Identity (Key Vault Secrets User)
 resource "azurerm_role_assignment" "keyvault_role_assignment" {
-  count               = length(azurerm_kubernetes_cluster.cluster) > 0 && length(azurerm_container_registry.container_registry) > 0 ? 1 : 0
-
+  count               = length(azurerm_kubernetes_cluster.cluster) > 0 ? 1 : 0
   principal_id        = azurerm_kubernetes_cluster.cluster[0].identity[0].principal_id
   role_definition_name = "Key Vault Secrets User"
-  scope               = azurerm_key_vault.key_vault[0].id  # Use [count.index] for the Key Vault
+  scope               = azurerm_key_vault.key_vault[0].id
 
   depends_on = [
     azurerm_kubernetes_cluster.cluster,
     azurerm_key_vault.key_vault
   ]
 }
-
-
-
-
