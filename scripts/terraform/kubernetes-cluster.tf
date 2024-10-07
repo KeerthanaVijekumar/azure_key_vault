b13132c117e4b610f90e51d9eb8051ec6d7e3e3d
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 17:39:23 +1100

    kube resource update

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -1,8 +1,8 @@
 # Create the Kubernetes cluster
 resource "azurerm_kubernetes_cluster" "cluster" {
   name                = var.app_name
-  location            = azurerm_resource_group.flixtubeazurekeyvault[0].location
-  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name
+  location            = azurerm_resource_group.flixtubeazurekeyvault.location
+  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault.name
   dns_prefix          = var.app_name
   kubernetes_version  = var.kubernetes_version
 
@@ -21,10 +21,9 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 
 # Assign Key Vault access to the AKS Managed Identity (Key Vault Secrets User)
 resource "azurerm_role_assignment" "keyvault_role_assignment" {
-  count               = length(azurerm_kubernetes_cluster.cluster) > 0 ? 1 : 0
-  principal_id        = azurerm_kubernetes_cluster.cluster[0].identity[0].principal_id
+  principal_id        = azurerm_kubernetes_cluster.cluster.identity[0].principal_id
   role_definition_name = "Key Vault Secrets User"
-  scope               = azurerm_key_vault.key_vault[0].id
+  scope               = azurerm_key_vault.key_vault.id
 
   depends_on = [
     azurerm_kubernetes_cluster.cluster,
@@ -34,10 +33,9 @@ resource "azurerm_role_assignment" "keyvault_role_assignment" {
 
 # Assign AKS cluster access to the Azure Container Registry (ACR) for pulling images
 resource "azurerm_role_assignment" "acr_role_assignment" {
-  count               = length(azurerm_kubernetes_cluster.cluster) > 0 ? 1 : 0
-  principal_id        = azurerm_kubernetes_cluster.cluster[0].identity[0].principal_id
+  principal_id        = azurerm_kubernetes_cluster.cluster.identity[0].principal_id
   role_definition_name = "AcrPull"
-  scope               = azurerm_container_registry.container_registry[0].id
+  scope               = azurerm_container_registry.container_registry.id
 
   depends_on = [
     azurerm_kubernetes_cluster.cluster,
