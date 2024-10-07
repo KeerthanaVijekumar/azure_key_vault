5d63fb1c1ab966b064a4edb630722ff1742cada0
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 17:19:03 +1100

    kubefile update

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -1,16 +1,16 @@
 # Check if the Kubernetes cluster exists
 data "azurerm_kubernetes_cluster" "existing_cluster" {
   name                = var.app_name
-  resource_group_name = var.app_name  # Use var.app_name instead of accessing resource group directly
-  depends_on          = [azurerm_resource_group.flixtubeazurekeyvault]  # Ensure resource group is created first
+  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name
+  depends_on          = [azurerm_resource_group.flixtubeazurekeyvault]
 }
 
 # Create the Kubernetes cluster only if it does not exist
 resource "azurerm_kubernetes_cluster" "cluster" {
   count               = length(data.azurerm_kubernetes_cluster.existing_cluster.id) == 0 ? 1 : 0
   name                = var.app_name
-  location            = azurerm_resource_group.flixtubeazurekeyvault.location  # No need for [0] index
-  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault.name      # No need for [0] index
+  location            = azurerm_resource_group.flixtubeazurekeyvault[0].location
+  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name
   dns_prefix          = var.app_name
   kubernetes_version  = var.kubernetes_version
 
@@ -32,10 +32,23 @@ resource "azurerm_role_assignment" "keyvault_role_assignment" {
   count               = length(azurerm_kubernetes_cluster.cluster) > 0 ? 1 : 0
   principal_id        = azurerm_kubernetes_cluster.cluster[0].identity[0].principal_id
   role_definition_name = "Key Vault Secrets User"
-  scope               = azurerm_key_vault.key_vault.id  # No need for [0] index
+  scope               = azurerm_key_vault.key_vault[0].id
 
   depends_on = [
     azurerm_kubernetes_cluster.cluster,
     azurerm_key_vault.key_vault
+  ]
+}
+
+# Assign AKS cluster access to the Azure Container Registry (ACR) for pulling images
+resource "azurerm_role_assignment" "acr_role_assignment" {
+  count               = length(azurerm_kubernetes_cluster.cluster) > 0 ? 1 : 0
+  principal_id        = azurerm_kubernetes_cluster.cluster[0].identity[0].principal_id
+  role_definition_name = "AcrPull"
+  scope               = azurerm_container_registry.container_registry[0].id
+
+  depends_on = [
+    azurerm_kubernetes_cluster.cluster,
+    azurerm_container_registry.container_registry
   ]
 }
