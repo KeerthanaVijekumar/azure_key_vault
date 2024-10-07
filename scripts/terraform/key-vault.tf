69f1b182cf8337c8329d5682a63faa37b599ceb2
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 15:53:21 +1100

    count added

--- scripts/terraform/key-vault.tf
+++ scripts/terraform/key-vault.tf
@@ -3,8 +3,7 @@ data "azurerm_client_config" "current" {}
 
 # Check if the Key Vault exists using the data block
 data "azurerm_key_vault" "existing_kv" {
-  name = var.app_name
-  # Conditional reference: Check if the resource group exists before accessing its attributes
+  name                = var.app_name
   resource_group_name = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? azurerm_resource_group.flixtubeazurekeyvault[0].name : var.app_name
 }
 
@@ -13,7 +12,7 @@ resource "azurerm_key_vault" "key_vault" {
   count               = length(data.azurerm_key_vault.existing_kv.id) == 0 ? 1 : 0  # Create if it doesn't exist
   name                = var.app_name
   location            = var.location
-  resource_group_name = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? azurerm_resource_group.flixtubeazurekeyvault[0].name : var.app_name  # Conditional access
+  resource_group_name = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? azurerm_resource_group.flixtubeazurekeyvault[0].name : var.app_name
   sku_name            = "standard"
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
@@ -44,9 +43,10 @@ resource "azuread_service_principal" "example" {
 
 # Assign Key Vault Secrets User role to the Service Principal
 resource "azurerm_role_assignment" "key_vault_role_assignment" {
-  principal_id = length(azuread_service_principal.example) > 0 ? azuread_service_principal.example[0].id : data.azuread_service_principal.existing_sp.id
+  count               = length(azurerm_key_vault.key_vault) > 0 ? 1 : 0  # Only assign if the Key Vault exists
+  principal_id        = length(azuread_service_principal.example) > 0 ? azuread_service_principal.example[0].id : data.azuread_service_principal.existing_sp.id
   role_definition_name = "Key Vault Secrets User"
-  scope               = azurerm_key_vault.key_vault[0].id  # Reference Key Vault directly
+  scope               = length(azurerm_key_vault.key_vault) > 0 ? azurerm_key_vault.key_vault[0].id : null  # Check if Key Vault exists
 
   depends_on = [azurerm_key_vault.key_vault]
 }
