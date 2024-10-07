28229538a39d0ee5be3a7181e21cbb8fb0d61fbc
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 17:27:47 +1100

    kubefile update

--- scripts/terraform/kubernetes-cluster.tf
+++ scripts/terraform/kubernetes-cluster.tf
@@ -1,16 +1,9 @@
-# Check if the Kubernetes cluster exists
-data "azurerm_kubernetes_cluster" "existing_cluster" {
-  name                = var.app_name
-  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name
-  depends_on          = [azurerm_resource_group.flixtubeazurekeyvault]
-}
-
-# Create the Kubernetes cluster only if it does not exist
+# Create the Kubernetes cluster
 resource "azurerm_kubernetes_cluster" "cluster" {
-  count               = length(data.azurerm_kubernetes_cluster.existing_cluster.id) == 0 ? 1 : 0
+  count               = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? 1 : 0
   name                = var.app_name
-  location            = azurerm_resource_group.flixtubeazurekeyvault[0].location
-  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name
+  location            = azurerm_resource_group.flixtubeazurekeyvault[count.index].location
+  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[count.index].name
   dns_prefix          = var.app_name
   kubernetes_version  = var.kubernetes_version
 
@@ -30,9 +23,9 @@ resource "azurerm_kubernetes_cluster" "cluster" {
 # Assign Key Vault access to the AKS Managed Identity (Key Vault Secrets User)
 resource "azurerm_role_assignment" "keyvault_role_assignment" {
   count               = length(azurerm_kubernetes_cluster.cluster) > 0 ? 1 : 0
-  principal_id        = azurerm_kubernetes_cluster.cluster[0].identity[0].principal_id
+  principal_id        = azurerm_kubernetes_cluster.cluster[count.index].identity[0].principal_id
   role_definition_name = "Key Vault Secrets User"
-  scope               = azurerm_key_vault.key_vault[0].id
+  scope               = azurerm_key_vault.key_vault[count.index].id
 
   depends_on = [
     azurerm_kubernetes_cluster.cluster,
@@ -43,9 +36,9 @@ resource "azurerm_role_assignment" "keyvault_role_assignment" {
 # Assign AKS cluster access to the Azure Container Registry (ACR) for pulling images
 resource "azurerm_role_assignment" "acr_role_assignment" {
   count               = length(azurerm_kubernetes_cluster.cluster) > 0 ? 1 : 0
-  principal_id        = azurerm_kubernetes_cluster.cluster[0].identity[0].principal_id
+  principal_id        = azurerm_kubernetes_cluster.cluster[count.index].identity[0].principal_id
   role_definition_name = "AcrPull"
-  scope               = azurerm_container_registry.container_registry[0].id
+  scope               = azurerm_container_registry.container_registry[count.index].id
 
   depends_on = [
     azurerm_kubernetes_cluster.cluster,
