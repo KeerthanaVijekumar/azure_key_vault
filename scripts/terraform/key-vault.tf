7a380860c90e03b8ba743cabaa8e585e1760da27
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 15:47:30 +1100

    keyvault file

--- scripts/terraform/key-vault.tf
+++ scripts/terraform/key-vault.tf
@@ -3,8 +3,9 @@ data "azurerm_client_config" "current" {}
 
 # Check if the Key Vault exists using the data block
 data "azurerm_key_vault" "existing_kv" {
-  name                = var.app_name
-  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name  # Use [0] to access the resource group attributes
+  name = var.app_name
+  # Conditional reference: Check if the resource group exists before accessing its attributes
+  resource_group_name = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? azurerm_resource_group.flixtubeazurekeyvault[0].name : var.app_name
 }
 
 # Only create the Key Vault if it doesn't already exist
@@ -12,7 +13,7 @@ resource "azurerm_key_vault" "key_vault" {
   count               = length(data.azurerm_key_vault.existing_kv.id) == 0 ? 1 : 0  # Create if it doesn't exist
   name                = var.app_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault[0].name  # Use [0] to access the resource group attributes
+  resource_group_name = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? azurerm_resource_group.flixtubeazurekeyvault[0].name : var.app_name  # Conditional access
   sku_name            = "standard"
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
@@ -43,7 +44,7 @@ resource "azuread_service_principal" "example" {
 
 # Assign Key Vault Secrets User role to the Service Principal
 resource "azurerm_role_assignment" "key_vault_role_assignment" {
-  principal_id        = length(azuread_service_principal.example) > 0 ? azuread_service_principal.example[0].id : data.azuread_service_principal.existing_sp.id
+  principal_id = length(azuread_service_principal.example) > 0 ? azuread_service_principal.example[0].id : data.azuread_service_principal.existing_sp.id
   role_definition_name = "Key Vault Secrets User"
   scope               = azurerm_key_vault.key_vault[0].id  # Reference Key Vault directly
 
