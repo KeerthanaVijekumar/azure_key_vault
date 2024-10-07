8771d9e057c0a341e4773e0f7eb021a333f5c1f8
KeerthanaVijekumar <s224719679@deakin.edu.au>
2024-10-07 15:44:25 +1100

    keyvault update

--- scripts/terraform/key-vault.tf
+++ scripts/terraform/key-vault.tf
@@ -1,14 +1,15 @@
-# Get the current Azure client configuration (tenant ID and client ID)
-data "azurerm_client_config" "current" {}
+# Check if the Key Vault exists using the data block
+data "azurerm_key_vault" "existing_kv" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault.name
+}
 
-# Create the Key Vault 
+# Only create the Key Vault if it doesn't already exist
 resource "azurerm_key_vault" "key_vault" {
+  count               = length(data.azurerm_key_vault.existing_kv.id) == 0 ? 1 : 0  # Create if it doesn't exist
   name                = var.app_name
   location            = var.location
-
-  # Check if the resource group was created and reference it conditionally
-  resource_group_name = length(azurerm_resource_group.flixtubeazurekeyvault) > 0 ? azurerm_resource_group.flixtubeazurekeyvault[0].name : var.app_name  # Conditional access
-
+  resource_group_name = azurerm_resource_group.flixtubeazurekeyvault.name
   sku_name            = "standard"
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
@@ -17,8 +18,9 @@ resource "azurerm_key_vault" "key_vault" {
     ignore_changes  = [sku_name, tenant_id]  # Ignore changes to sku_name and tenant_id
   }
 
-  depends_on = [azurerm_resource_group.flixtubeazurekeyvault]  # Ensure resource creation order
+  depends_on = [azurerm_resource_group.flixtubeazurekeyvault]
 }
+
 
 # Check if the service principal already exists using the client ID from the current Azure configuration
 data "azuread_service_principal" "existing_sp" {
